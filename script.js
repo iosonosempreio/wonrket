a3ca43979111dff8f7a3f4c408bf8344e1919432
iosonosempreio <tommaso.elli@gmail.com>
2018-05-03 07:44:51 +0200

    update interface

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ let nodes = [],
 
 let forceConfig = {
     manyBody: {
-        strength: -30
+        strength: -30,
+        distanceMin: 1,
+        distanceMax: 2000
+    },
+    links: {
+        strength: 0
     }
 }
+
+forceConfig.manyBody.strength
 
 function handleFileSelect(evt) {
     target = evt.target;
@@ -70,7 +77,7 @@ function loaded(evt) {
             .html('')
             .classed('filled', true)
             .html(`
-              <span>Selected ${nodes.length} nodes</span>
+              <span>Loaded ${nodes.length} nodes</span>
               <span class="clear nodes"><i class="fas fa-trash-alt"></i></span>
               `);
         d3.select('.clear.nodes')
@@ -89,11 +96,20 @@ function loaded(evt) {
             })
     } else if (table == 'edges') {
         links = fileString;
+        let maxWeight = d3.max(links, function(d){return d.value})
+        links = links.map(function(d){
+            return {
+                source: d.source,
+                target: d.target,
+                weight: d.value/maxWeight
+            }
+        })
+        console.log(links);
         d3.select(target)
             .html('')
             .classed('filled', true)
             .html(`
-              <span>Selected ${links.length} edges</span>
+              <span>Loaded ${links.length} edges</span>
               <span class="clear edges"><i class="fas fa-trash-alt"></i></span>
               `);
         d3.select('.clear.edges')
@@ -144,23 +160,24 @@ let height = svg.node()
     .height
 
 let simulation = d3.forceSimulation(nodes)
-    .force("center", d3.forceCenter(width/2, height/2))
+    .force("center", d3.forceCenter(width / 2, height / 2))
     .force("link", d3.forceLink(links)
         .id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody()
-        .strength(forceConfig.manyBody.strength))
-    .force("x", d3.forceX(width/2))
-    .force("y", d3.forceY(height/2))
-    .alphaMin(0.05)
+    .force("charge", d3.forceManyBody())
+    .force("x", d3.forceX(width / 2))
+    .force("y", d3.forceY(height / 2))
+    .alphaMin(0.0)
+    .alpha(0)
     .on("tick", ticked);
 
-d3.select('.restart-force').on('click', function(d){
-  restart();
-})
+d3.select('.restart-force')
+    .on('click', function(d) {
+        restart();
+    })
 
 let g = svg.append("g")
-    // .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
-    link = g.append("g")
+// .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")"),
+link = g.append("g")
     .attr("stroke", "#000")
     .attr("stroke-width", 1.5)
     .selectAll(".link"),
@@ -173,13 +190,15 @@ function restart() {
 
     console.log(`There are ${nodes.length} nodes and ${links.length} links.`);
 
+    d3.select('body').classed('network-loaded', true);
+
     // Apply the general update pattern to the nodes.
     node = node.data(nodes, function(d) { return d.id; });
     node.exit()
         .remove();
     node = node.enter()
         .append("circle")
-        .attr("fill", function(d) { return 'black'; })
+        .attr("fill", function(d) { return 'tomato'; })
         .attr("r", 4)
         .merge(node);
 
@@ -189,12 +208,20 @@ function restart() {
         .remove();
     link = link.enter()
         .append("line")
+        .attr('stroke', '#5D6D7E')
         .merge(link);
 
     // Update and restart the simulation.
-    simulation.nodes(nodes);
-    simulation.force("link")
-        .links(links);
+    simulation.nodes(nodes)
+    simulation.force("link").links(links)
+    simulation.force("charge")
+        .strength(forceConfig.manyBody.strength)
+        .distanceMin(forceConfig.manyBody.distanceMin)
+        .distanceMax(forceConfig.manyBody.distanceMax)
+        .distanceMax(100000)
+
+
+
     simulation.alpha(1)
         .restart();
 }
@@ -208,27 +235,30 @@ function ticked() {
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
 
-    d3.select('.decay-bar').style('width', simulation.alpha()*100 + '%')
+    d3.select('.decay-bar')
+        .style('width', simulation.alpha() * 100 + '%')
 }
 
 //add zoom capabilities 
 var zoom_handler = d3.zoom()
-    .on("zoom", zoom_actions);
+    .on("zoom", function() {
+        if (true) {
+            zoom_actions();
+        }
+    });
 
 //Zoom functions 
-function zoom_actions(){
+function zoom_actions() {
     g.attr("transform", d3.event.transform)
 }
-
-zoom_handler(svg);   
-
+zoom_handler(svg);
 
 // Sliders
 
 var slider1 = d3.sliderHorizontal()
     .min(-100)
     .max(100)
-    .width(300)
+    .width(250)
     // .tickFormat(d3.format('.2%'))
     .step(1)
     .ticks(5)
@@ -236,9 +266,7 @@ var slider1 = d3.sliderHorizontal()
     .on('onchange', val => {
         d3.select("p#value1")
             .text(val);
-        simulation.force("charge", d3.forceManyBody()
-            .strength(val))
-
+        forceConfig.manyBody.strength = val;
         restart();
     });
 
@@ -258,17 +286,17 @@ d3.select("#resetValue1")
 
 
 var slider2 = d3.sliderHorizontal()
-    .min(0)
-    .max(2)
-    .width(300)
+    .min(1)
+    .max(500)
+    .width(250)
     // .tickFormat(d3.format('.2%'))
+    .step(1)
     .ticks(5)
-    .default(1)
+    .default(forceConfig.manyBody.distanceMin)
     .on('onchange', val => {
         d3.select("p#value2")
             .text(val);
-        simulation.force("link", d3.forceLink(links).strength(val))
-
+        forceConfig.manyBody.distanceMin = val;
         restart();
     });
 
@@ -283,5 +311,35 @@ gSlider2.call(slider2);
 
 d3.select("p#value2")
     .text((slider2.value()))
-d3.select("#resetValue2")
-    .on("click", () => slider2.value(0));
+d3.select("#resetValue1")
+    .on("click", () => slider2.value(1));
+
+
+var slider3 = d3.sliderHorizontal()
+    .min(1)
+    .max(2000)
+    .width(250)
+    // .tickFormat(d3.format('.2%'))
+    .step(1)
+    .ticks(5)
+    .default(2000)
+    .on('onchange', val => {
+        d3.select("p#value3")
+            .text(val);
+        forceConfig.manyBody.distanceMax = val;
+        restart();
+    });
+
+var gSlider3 = d3.select("div#slider3")
+    .append("svg")
+    .attr("width", 500)
+    .attr("height", 100)
+    .append("g")
+    .attr("transform", "translate(30,30)");
+
+gSlider3.call(slider3);
+
+d3.select("p#value3")
+    .text((slider3.value()))
+d3.select("#resetValue1")
+    .on("click", () => slider3.value(2000));
